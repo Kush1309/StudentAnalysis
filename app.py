40d4e01d804ee5ad2bde17269f4ebdc0a66bc25b
Kushagra Saxena <kushagrasaxena0913@gmail.com>
2025-12-19 23:46:06 +0530

    remove apndas

--- app.py
+++ app.py
@@ -1,25 +1,4 @@
-<<<<<<< HEAD
-import streamlit as st
-import pandas as pd
-import numpy as np
-import joblib
-import warnings
-warnings.filterwarnings("ignore")
-model= joblib.load("best_model.pkl")
-st.title("Student Exam Score Predictor")
-study_hours = st.slider("Study Hours per Day", 0.0, 12.0, 2.0)
-attendance=st.slider("Attendance Percentage", 0.0, 100.0, 80.0)
-mental_health=st.slider("Mental Health Rating (1-10)", 1, 10, 5)
-sleep_hours = st.slider("Sleep Hours per Night", 0.0, 12.0, 7.0)
-part_time_job = st.selectbox("Part-Time Job", ["No", "Yes"])
-ptj_encoded = 1 if part_time_job== "Yes" else 0
-if st.button("Predict Exam Score"):
-    input_data= np.array([[study_hours, attendance,mental_health,sleep_hours,ptj_encoded]])
-    prediction=model.predict(input_data)[0]
-    prediction=max(0, min(100,prediction))
-    st.success(f"Predicted Exam Score: {prediction:.2f}")
-=======
-<<<<<<< HEAD
+
 import streamlit as st
 import numpy as np
 import joblib
@@ -38,25 +17,6 @@ if st.button("Predict Exam Score"):
     prediction=model.predict(input_data)[0]
     prediction=max(0, min(100,prediction))
     st.success(f"Predicted Exam Score: {prediction:.2f}")
-=======
-import streamlit as st
-import numpy as np
-import joblib
-import warnings
-warnings.filterwarnings("ignore")
-model= joblib.load("best_model.pkl")
-st.title("Student Exam Score Predictor")
-study_hours = st.slider("Study Hours per Day", 0.0, 12.0, 2.0)
-attendance=st.slider("Attendance Percentage", 0.0, 100.0, 80.0)
-mental_health=st.slider("Mental Health Rating (1-10)", 1, 10, 5)
-sleep_hours = st.slider("Sleep Hours per Night", 0.0, 12.0, 7.0)
-part_time_job = st.selectbox("Part-Time Job", ["No", "Yes"])
-ptj_encoded = 1 if part_time_job== "Yes" else 0
-if st.button("Predict Exam Score"):
-    input_data= np.array([[study_hours, attendance,mental_health,sleep_hours,ptj_encoded]])
-    prediction=model.predict(input_data)[0]
-    prediction=max(0, min(100,prediction))
-    st.success(f"Predicted Exam Score: {prediction:.2f}")
->>>>>>> a11c3c61aa047774e09bfd3a3707516ee226c18b
->>>>>>> a900ca4592403decee52eafc54bd65ca0ff47f2d
+
+
     
